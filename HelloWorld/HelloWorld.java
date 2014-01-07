25720e6988877f2d0c12afe357d83f21051fcbbe
Aniket Pant <me@aniketpant.com>
2014-01-07 20:43:21 +0530

    first commit

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,8 @@
+// HelloWorld.java
+// The start of every coding exercise
+
+public class HelloWorld {
+   public static void main(String[] args) {
+      System.out.println("Hello, World!");
+   }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+Learn Java
+==========
+
+This repository contains code samples which I have written and borrowed from places in an attempt to learn Java.
